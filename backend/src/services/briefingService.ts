73883383444cdc3c66b75e603eb700315c501fca
Yon Liud <ejliud@gmail.com>
2026-03-20 18:48:51 +0200

    feat: briefings service

---
+++ backend/src/services/briefingService.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../lib/prisma.ts"
+import { generateId } from "../lib/nanoid.ts"
+
+export const briefingService = {
+    getAll: async () => {
+        return await prisma.briefing.findMany()        
+    },
+    getById: async (briefingId: string) => {
+        return await prisma.briefing.findUnique({
+            where: {
+                briefing_id: briefingId
+            }
+        })
+    },
+    getByUser: async (userId: string) => {
+        return await prisma.briefing.findMany({
+            where: {user_id: userId}
+        })
+    },
+    create: async (data: { user_id: string, full_summary: string, short_summary: string }) => {
+        const briefingId = generateId()
+
+        return await prisma.briefing.create({
+            data: {
+                briefing_id: briefingId,
+                ...data
+            }
+        })
+    },
+    update: async (briefingId: string, data: Partial<{ full_summary: string; short_summary: string }>) => {
+        return await prisma.briefing.update({
+            where: { briefing_id: briefingId },
+            data
+        })
+    },
+
+    delete: async (briefingId: string) => {
+        return await prisma.briefing.delete({
+            where: { briefing_id: briefingId }
+        })
+    }
+
+}
