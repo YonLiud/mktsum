7b5923f0648e9e0a664dd68652ece3241d336246
Yon Liud <ejliud@gmail.com>
2026-05-08 20:42:01 +0300

    misc: remove auto redriect on logged on users

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,6 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { HomePage } from '@/pages/HomePage'
 
 export const Route = createFileRoute('/')({
-  beforeLoad: () => {
-    if (localStorage.getItem('auth_user')) {
-      throw redirect({ to: '/dashboard' })
-    }
-  },
   component: HomePage,
 })
